fbb53884b9bc3ffddb30866c02ceaf9a2cdcc81c
jammjakx <ralphverspui@live.nl>
2016-11-08 20:34:24 +0100

    Add files via upload

---
+++ MovieOrganizer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MovieOrganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importMoviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbMovieNames = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(496, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importMoviesToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importMoviesToolStripMenuItem
+            // 
+            this.importMoviesToolStripMenuItem.Name = "importMoviesToolStripMenuItem";
+            this.importMoviesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.importMoviesToolStripMenuItem.Text = "Import Movies";
+            this.importMoviesToolStripMenuItem.Click += new System.EventHandler(this.importMoviesToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // lbMovieNames
+            // 
+            this.lbMovieNames.FormattingEnabled = true;
+            this.lbMovieNames.Location = new System.Drawing.Point(12, 27);
+            this.lbMovieNames.Name = "lbMovieNames";
+            this.lbMovieNames.Size = new System.Drawing.Size(151, 251);
+            this.lbMovieNames.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 289);
+            this.Controls.Add(this.lbMovieNames);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Movie Organizer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importMoviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ListBox lbMovieNames;
+    }
+}
+
---
+++ MovieOrganizer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace MovieOrganizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void importMoviesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var listboxItems = getDirectoryNames(getPathName());
+            foreach (var item in listboxItems)
+            {
+                lbMovieNames.Items.Add(item);
+            }
+        }
+
+        private string getPathName()
+        {
+            var fbd = new FolderBrowserDialog();
+
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                return fbd.SelectedPath;
+            }
+            return null; 
+        }
+
+        private List<String> getDirectoryNames(string pathName)
+        {
+            var directoryNames = new List<String>();
+            var directories = Directory.GetDirectories(pathName, "*", SearchOption.TopDirectoryOnly);
+
+            foreach (var directory in directories)
+            {
+               directoryNames.Add(Path.GetFileName(directory));
+            }
+            return directoryNames;
+        }
+        
+    }
+}
